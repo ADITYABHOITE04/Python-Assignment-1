135961813573a303d5ace22bcf3bf6c853d1c67e
Aditya Bhoite <143338311+ADITYABHOITE04@users.noreply.github.com>
2026-06-13 03:10:19 +0530

    Delete function.py

--- function.py
+++
@@ -1,9 +0,0 @@
-# ASSIGNMENT 1 - QUE 1
-
-num = 10
-name = "Aditya"
-pi = 3.14
-
-print("Value :", num, "Type :", type(num), "Address :", id(num))
-print("Value :", name, "Type :", type(name), "Address :", id(name))
-print("Value :", pi, "Type :", type(pi), "Address :", id(pi))
