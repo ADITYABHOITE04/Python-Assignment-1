393c344429020fdce7c195223d759fa037dd10cf
Aditya Bhoite <143338311+ADITYABHOITE04@users.noreply.github.com>
2026-06-13 03:05:59 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,9 @@
+# ASSIGNMENT 1 - QUE 1
+
+num = 10
+name = "Aditya"
+pi = 3.14
+
+print("Value :", num, "Type :", type(num), "Address :", id(num))
+print("Value :", name, "Type :", type(name), "Address :", id(name))
+print("Value :", pi, "Type :", type(pi), "Address :", id(pi))
